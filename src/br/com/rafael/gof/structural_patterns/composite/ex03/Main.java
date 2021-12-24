89b48941a6265211694b57df44a27140ff575228
Fael123Programming <rafaelfonseca1020@gmail.com>
2021-12-24 11:04:20 -0300

    New System.out.println() line

--- src/br/com/rafael/gof/structural_patterns/composite/ex03/Main.java
+++ src/br/com/rafael/gof/structural_patterns/composite/ex03/Main.java
@@ -1,11 +1,14 @@
 package br.com.rafael.gof.structural_patterns.composite.ex03;
 
 import br.com.rafael.gof.structural_patterns.composite.ex03.composite.Box;
-import br.com.rafael.gof.structural_patterns.composite.ex03.leaves.*;
+import br.com.rafael.gof.structural_patterns.composite.ex03.leaves.TShirt;
+import br.com.rafael.gof.structural_patterns.composite.ex03.leaves.Trouser;
+
 import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Composite pattern is very handy");
         Box boxOfTShirts = new Box();
         boxOfTShirts.insert(new TShirt(new BigDecimal("200.49")));
         boxOfTShirts.insert(new TShirt(new BigDecimal("400")));
